646ce10de4686251e177878c497403b888b4e21b
TatianaApatchenko <tanya.apatchenko@gmail.com>
2025-03-12 00:26:22 +0300

    Добавлена проверка отбрас. треуг. с отриц. стор. + наруш. нерав. треуг.

--- sandbox/src/main/java/ru/apatch/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/apatch/geometry/figures/Triangle.java
@@ -1,43 +1,39 @@
 package ru.apatch.geometry.figures;
 
-public class Triangle {
-
-    static double a;
-    static double b;
-    static double c;
-
-    public Triangle(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
-
+public record Triangle(
+        double a,
+        double b,
+        double c
+) {
+    public Triangle {
+        if (a < 0 || b < 0 || c < 0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalArgumentException("Triangle inequality is violated");
+        }
     }
 
     public static void printTriangleArea(Triangle s) {
-        String text = String.format("Area of triangle with sides %f , %f, %f = %f", s.a, s.b, s.c, s.area());
+        String text = String.format("Area of triangle with sides %f, %f, %f = %f", s.a, s.b, s.c, s.area());
         System.out.println(text);
-
     }
 
     public double area() {
-        double pp = semiPerimetr();
-        return Math.sqrt(pp*(pp - this.a)*(pp - this.b)*(pp - this.c));
+        double pp = semiPerimeter();
+        return Math.sqrt(pp * (pp - this.a) * (pp - this.b) * (pp - this.c));
     }
-
 
     public static void printTrianglePerimeter(Triangle p) {
-        String text = String.format("Perimeter of triangle with sides %f , %f, %f = %f", p.a, p.b, p.c, p.perimeter());
+        String text = String.format("Perimeter of triangle with sides %f, %f, %f = %f", p.a, p.b, p.c, p.perimeter());
         System.out.println(text);
-
     }
+
     public double perimeter() {
         return this.a + this.b + this.c;
     }
 
-    public double semiPerimetr(){
-        return perimeter()/2;
+    public double semiPerimeter() {
+        return perimeter() / 2;
     }
-
 }
-
-
--- sandbox/src/test/java/ru/apatch/geometry/figures/TriangleTest.java
+++ sandbox/src/test/java/ru/apatch/geometry/figures/TriangleTest.java
@@ -12,11 +12,9 @@ double result = s.area();
      Assertions.assertEquals(28.61817604250837, new Triangle(10.0,6.0,10.0).area());
      Assertions.assertEquals(7.483314773547883, new Triangle(5.0,3.0,6.0).area());
 
-
-
  }
     @Test
-    public void canCalculatePerimetr() {
+    public void canCalculatePerimeter() {
      var p = new Triangle(5.0,4.0,3.0);
      double result = p.perimeter();
         Assertions.assertEquals(12.0, new Triangle(5.0,4.0,3.0).perimeter());
@@ -25,5 +23,27 @@ double result = s.area();
 
 
     }
-
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-5, 3, 4);
+            new Triangle(4, -3, 4);
+            new Triangle(4, 3, -4);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+ }
+    @Test
+    void cannotCreateTriangleWithInvalidSides() {
+        try {
+            new Triangle(1, 2, 3);
+            new Triangle(2, 3, 6);
+            new Triangle(2, 3, 6);
+            Assertions.fail("Expected IllegalArgumentException for triangle inequality violation");
+        } catch (IllegalArgumentException exception) {
+            // OK
+        }
 }
+
+    }
