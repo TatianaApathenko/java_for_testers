25711a4b9c33ca49a303409474c6bdfaee4b8ca4
apatch <tanya.apatchenko@gmail.com>
2025-03-27 18:26:55 +0300

    Дополнена функция equales

--- sandbox/src/test/java/ru/apatch/geometry/figures/TriangleTest.java
+++ sandbox/src/test/java/ru/apatch/geometry/figures/TriangleTest.java
@@ -50,6 +50,7 @@ double result = s.area();
     void TestEquality(){
      var t1 = new Triangle(3.0,4.0,5.0);
      var t2 =  new Triangle(4.0,5.0, 3.0);
+    var t3 =  new Triangle(5.0,3.0, 4.0);
     Assertions.assertEquals(t1, t2);
 }
 
