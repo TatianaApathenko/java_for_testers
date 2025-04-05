93f121294ba68fd5486474f997c1696e89135dd7
TatianaApatchenko <tanya.apatchenko@gmail.com>
2025-04-05 12:14:43 +0300

    9 задание. Параметризовать тесты для создания групп и контактов

--- addressbook_web_tests/src/test/java/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/manager/ContactHelper.java
@@ -2,57 +2,62 @@ package manager;
 
 import model.ContactData;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 
-public class ContactHelper extends HelperBase {
+public class ContactHelper extends HelperBase{
 
     public ContactHelper(ApplicationManager manager) {
         super(manager);
     }
 
-    public void createContact(ContactData contact) {
-        manager.getDriver().findElement(By.linkText("add new")).click();
+    public void CreateContact(ContactData contact) {
         fillContactForm(contact);
-        manager.getDriver().findElement(By.cssSelector("input[type='submit']")).click();
-        manager.getDriver().findElement(By.linkText("home page")).click();
-    }
-
-    private void fillContactForm(ContactData contact) {
-        manager.getDriver().findElement(By.name("firstname")).sendKeys(contact.firstname());
-        manager.getDriver().findElement(By.name("lastname")).sendKeys(contact.lastname());
-        manager.getDriver().findElement(By.name("address")).sendKeys(contact.address());
-        manager.getDriver().findElement(By.name("mobile")).sendKeys(contact.mobile());
-        manager.getDriver().findElement(By.name("work")).sendKeys(contact.work());
-        manager.getDriver().findElement(By.name("email")).sendKeys(contact.email());
+        returnToHomePAge();
     }
 
     public void removeContact() {
-        manager.getDriver().findElement(By.name("selected[]")).click();
-        manager.getDriver().findElement(By.cssSelector(".left:nth-child(8) input[type='button'][value='Delete']")).click();
-        acceptAlert();
+        click(By.name("selected[]"));
+        deleteContacts();
+        click(By.id("logo"));
     }
 
-    private void acceptAlert() {
-        if (isAlertPresent()) {
-            manager.getDriver().switchTo().alert().accept();
+    private void fillContactForm(ContactData contact) {
+        click(By.linkText("add new"));
+        type(By.name("firstname"), contact.firstname());
+        type(By.name("lastname"), contact.lastname());
+        type(By.name("address"), contact.address());
+        type(By.name("mobile"), contact.mobile());
+        click(By.name("theform"));
+        type(By.name("email"), contact.email());
+        submitContactCreation(By.name("submit"));
+    }
+
+    private void submitContactCreation(By locator) {
+
+        manager.driver.findElement(locator).click();
+    }
+
+    private void returnToHomePAge() {
+        manager.driver.findElement(By.linkText("home page")).click();
+    }
+
+    private void deleteContacts() {
+        manager.driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
+        //manager.driver.switchTo().alert().accept();
+    }
+
+    public int getCount() {
+        return manager.driver.findElements(By.name("selected[]")).size();
+    }
+
+    public void removeAllContacts() {
+        selectAllContacts();
+        deleteContacts();
+    }
+
+    private void selectAllContacts() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes) {
+            checkbox.click();
         }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            manager.getDriver().switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    public boolean isContactPresent() {
-        return isElementPresent(By.name("selected[]"));
-    }
-
-    private boolean isElementPresent(By locator) {
-        return manager.isElementPresent(locator);
     }
 }
-
--- addressbook_web_tests/src/test/java/model/ContactData.java
+++ addressbook_web_tests/src/test/java/model/ContactData.java
@@ -1,15 +1,27 @@
 package model;
 
-public record ContactData(String firstname, String lastname, String address, String mobile, String work, String email) {
+public record ContactData(String firstname, String lastname, String address, String mobile, String email) {
     public ContactData() {
-        this("", "", "", "", "", "");
+        this("", "", "", "", "");
     }
 
-    public ContactData withFirstname(String firstname) {
-        return new ContactData(firstname, this.lastname, this.address, this.mobile, this.work, this.email);
+    public ContactData withName(String firstname) {
+        return new ContactData(firstname, this.lastname, this.address, this.mobile, this.email);
+    }
+
+    public ContactData withLastName(String lastname) {
+        return new ContactData(this.firstname, lastname, this.address, this.mobile, this.email);
+    }
+
+    public ContactData withAddress(String address) {
+        return new ContactData(this.firstname, this.lastname, address, this.mobile, this.email);
     }
 
     public ContactData withMobile(String mobile) {
-        return new ContactData(this.firstname, this.lastname, this.address, mobile, this.work, this.email);
+        return new ContactData(this.firstname, this.lastname, this.address, mobile, this.email);
+    }
+
+    public ContactData withEmail(String email) {
+        return new ContactData(this.firstname, this.lastname, this.address, this.mobile, email);
     }
 }
