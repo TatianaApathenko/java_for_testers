81625a288785d1985af6f61464079c0494dfc994
apatch <tanya.apatchenko@gmail.com>
2025-04-10 17:35:32 +0300

    Доработка 10-11

--- addressbook_web_tests/src/test/java/manager/ContactHelper.java
+++ addressbook_web_tests/src/test/java/manager/ContactHelper.java
@@ -1,9 +1,7 @@
 package manager;
 
 import model.ContactData;
-import model.GroupData;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,70 +16,65 @@ public class ContactHelper extends HelperBase{
         click(By.linkText("add new"));
         fillContactForm(contact);
         submitContactCreation(By.name("submit"));
-        returnToHomePAge();
+        returnToHomePage();
     }
 
     public void removeContact(ContactData contact) {
-        click(By.cssSelector(String.format("input[value='%s']", contact.id())));
+        selectContact(contact);
         deleteContacts();
-        click(By.id("logo"));
+        returnToHomePage();
     }
+
     public void removeAllContacts() {
         selectAllContacts();
         deleteContacts();
     }
 
     public void modifyContact(ContactData contact, ContactData modifiedContact) {
-        returnToHomePAge();
+        returnToHomePage();
         selectContact(contact);
         initContactModification(contact);
         fillContactForm(modifiedContact);
         updateContactModification();
-        click(By.linkText("home page"));
+        returnToHomePage();
     }
-    private void updateContactModification() {
-        click(By.xpath("//input[@name='update']"));
 
+    private void updateContactModification() {
+        click(By.name("update"));
     }
+
     private void initContactModification(ContactData contact) {
         var edit = manager.driver.findElement(By.xpath(String.format("//a[@href='edit.php?id=%s']", contact.id())));
         edit.click();
     }
+
     private void selectContact(ContactData contact) {
         click(By.cssSelector(String.format("input[value='%s']", contact.id())));
     }
 
-
     private void fillContactForm(ContactData contact) {
-        click(By.linkText("add new"));
         type(By.name("firstname"), contact.firstname());
         type(By.name("lastname"), contact.lastname());
         type(By.name("address"), contact.address());
         type(By.name("mobile"), contact.mobile());
-        //click(By.name("theform"));
         type(By.name("email"), contact.email());
-        //submitContactCreation(By.name("submit"));
     }
 
     private void submitContactCreation(By locator) {
-
         manager.driver.findElement(locator).click();
     }
 
-    private void returnToHomePAge() {
-        manager.driver.findElement(By.linkText("home page")).click();
+    private void returnToHomePage() {
+        manager.driver.findElement(By.linkText("home")).click();
     }
 
     private void deleteContacts() {
         click(By.cssSelector(".left:nth-child(8) > input"));
-        //manager.driver.findElement(By.cssSelector(".left:nth-child(8) > input")).click();
-        //manager.driver.switchTo().alert().accept();
     }
 
     public int getCount() {
         return manager.driver.findElements(By.name("selected[]")).size();
     }
-
 
     private void selectAllContacts() {
         var checkboxes = manager.driver.findElements(By.name("selected[]"));
@@ -89,17 +82,18 @@ public class ContactHelper extends HelperBase{
             checkbox.click();
         }
     }
+
     public List<ContactData> getList() {
-        returnToHomePAge();
+        returnToHomePage();
         var contacts = new ArrayList<ContactData>();
         var trs = manager.driver.findElements(By.name("entry"));
-        for (var tr: trs){
+        for (var tr : trs) {
             var td = tr.findElements(By.tagName("td"));
-            String id = String.valueOf(Integer.parseInt(td.get(0).findElement(By.cssSelector("input[type='checkbox']")).getAttribute("value")));
+            String id = td.get(0).findElement(By.cssSelector("input[type='checkbox']")).getAttribute("value");
             String lastname = td.get(1).getText();
             String firstname = td.get(2).getText();
             contacts.add(new ContactData().withId(id).withName(firstname).withLastName(lastname));
         }
         return contacts;
-}
     }
+}
--- addressbook_web_tests/src/test/java/tests/ContactModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactModificationTests.java
@@ -14,21 +14,29 @@ public class ContactModificationTests extends TestBase{
     @Test
     void canModifyContact(){
         if (app.contacts().getCount() == 0){
-            app.contacts().CreateContact(new ContactData("","Irishka", "Mask", "Gdetotam", "+0999923321444", "irishka@mail.com"));
+            app.contacts().CreateContact(new ContactData("1", "Irishka", "Mask", "Gdetotam", "+0999923321444", "irishka@mail.com"));
         }
         var oldContacts = app.contacts().getList();
         var rnd = new Random();
         var index = rnd.nextInt(oldContacts.size());
-        var testData = new ContactData().withName("modified name");
-        app.contacts().modifyContact(oldContacts.get(index), testData);
+        var modifiedContact = new ContactData()
+                .withId(oldContacts.get(index).id())
+                .withName("Luda")
+                .withLastName(oldContacts.get(index).lastname())
+                .withAddress(oldContacts.get(index).address())
+                .withMobile(oldContacts.get(index).mobile())
+                .withEmail(oldContacts.get(index).email());
+
+        app.contacts().modifyContact(oldContacts.get(index), modifiedContact);
+
         var newContacts = app.contacts().getList();
         var expectedList = new ArrayList<>(oldContacts);
-        expectedList.set(index, testData.withId(oldContacts.get(index).id()));
-        Comparator<ContactData> compareById = (o1, o2) -> {
-            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
-        };
+        expectedList.set(index, modifiedContact);
+
+        Comparator<ContactData> compareById = (o1, o2) -> Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         newContacts.sort(compareById);
         expectedList.sort(compareById);
+
         Assertions.assertEquals(newContacts, expectedList);
     }
 }
