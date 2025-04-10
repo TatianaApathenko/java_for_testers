c672e4d269676df2485655b7cc0854c85b0c2249
apatch <tanya.apatchenko@gmail.com>
2025-04-10 17:58:34 +0300

    Модификация CreateContactTest

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,8 +1,8 @@
+
 package tests;
 
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -14,20 +14,11 @@ public class ContactCreationTests extends TestBase {
 
     public static List<ContactData> contactProvider() {
         var result = new ArrayList<ContactData>();
-        for (var firstname : List.of("", "firstname")) {
-            for (var lastname : List.of("", "lastname")) {
-                for (var address : List.of("", "address")) {
-                    for (var mobile : List.of("", "mobile")) {
-                        for (var email : List.of("", "email")) {
-                            result.add(new ContactData()
-                                    .withName(firstname)
-                                    .withLastName(lastname)
-                                    .withMobile(mobile)
-                                    .withAddress(address)
-                                    .withEmail(email));
-                        }
-                    }
-                }
+        for (var firstname : List.of("", "Luda")){
+            for (var lastname : List.of("", "Ed")){
+                result.add(new ContactData()
+                        .withName(firstname)
+                        .withLastName(lastname));
             }
         }
         for (int i = 0; i < 5; i++) {
@@ -44,19 +35,19 @@ public class ContactCreationTests extends TestBase {
         var oldContacts = app.contacts().getList();
         app.contacts().CreateContact(contact);
         var newContacts = app.contacts().getList();
-        Comparator<ContactData> compareById = Comparator.comparingInt(contactData -> Integer.parseInt(contactData.id()));
+        Comparator<ContactData> compareById = (o1, o2) -> {
+            return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
+        };
         newContacts.sort(compareById);
-
         var expectedList = new ArrayList<>(oldContacts);
         expectedList.add(contact.withId(newContacts.get(newContacts.size() - 1).id()));
         expectedList.sort(compareById);
-
-        Assertions.assertEquals(expectedList, newContacts);
+        Assertions.assertEquals(newContacts, expectedList);
     }
 
     public static List<ContactData> negativeContactProvider() {
-        var result = new ArrayList<ContactData>();
-        result.add(new ContactData().withName("").withLastName("contact'").withMobile("").withAddress("").withEmail(""));
+        var result = new ArrayList<ContactData>(List.of(
+                new ContactData("", "contact'", "", "", "", "")));
         return result;
     }
 
