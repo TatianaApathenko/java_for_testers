ef915306e3dee6ea0c66e395000f6399f4bb75f9
TatianaApatchenko <tanya.apatchenko@gmail.com>
2025-04-21 01:22:12 +0300

    Процесс выполнения задания 14

--- addressbook_web_tests/src/test/java/ru/apatch/addressbook/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/ru/apatch/addressbook/manager/HibernateHelper.java
@@ -20,7 +20,7 @@ public class HibernateHelper extends HelperBase {
         sessionFactory = new Configuration()
                 .addAnnotatedClass(GroupRecord.class)
                 .addAnnotatedClass(ContactRecord.class)
-                .setProperty(AvailableSettings.URL, "jdbc:mysql://localhost/addressbook")
+                .setProperty(AvailableSettings.URL, "jdbc:mysql://localhost/addressbook?zeroDateTimeBehavior=convertToNull")
                 .setProperty(AvailableSettings.USER, "root")
                 .setProperty(AvailableSettings.PASS, "")
                 .buildSessionFactory();
@@ -38,10 +38,32 @@ public class HibernateHelper extends HelperBase {
         return new GroupData("" + record.id, record.name, record.header, record.footer);
     }
 
+    private static GroupRecord convert(GroupData data) {
+        var id = data.id();
+        if ("".equals(id)) {
+            id = "0";
+        }
+        return new GroupRecord(Integer.parseInt(id), data.name(), data.header(), data.footer());
+    }
+
     public List<GroupData> getGroupList() {
         return convertList(sessionFactory.fromSession(session -> {
             return session.createQuery("from GroupRecord", GroupRecord.class).list();
         }));
+    }
+
+    public long getGroupCount() {
+        return sessionFactory.fromSession(session -> {
+            return session.createQuery("select count (*) from GroupRecord", Long.class).getSingleResult();
+        });
+    }
+
+    public void createGroup(GroupData groupData) {
+        sessionFactory.inSession(session -> {
+            session.getTransaction().begin();
+            session.persist(convert(groupData));
+            session.getTransaction().commit();
+        });
     }
 
     public List<ContactData> getContactList() {
@@ -65,5 +87,34 @@ public class HibernateHelper extends HelperBase {
                 .withAddress(record.address)
                 .withMobile(record.mobile)
                 .withEmail(record.email);
+    }
+
+    private static ContactRecord convertContact(ContactData data) {
+        var id = data.id();
+        if ("".equals(id)) {
+            id = "0";
+        }
+        return new ContactRecord(Integer.parseInt(id), data.firstname(), data.lastname(), data.address(), data.mobile(), data.email());
+    }
+
+
+    public long getContactCount() {
+        return sessionFactory.fromSession(session -> {
+            return session.createQuery("select count (*) from ContactRecord", Long.class).getSingleResult();
+        });
+    }
+
+    public void CreateContact(ContactData contactData) {
+        sessionFactory.inSession(session -> {
+            session.getTransaction().begin();
+            session.persist(convertContact(contactData));
+            session.getTransaction().commit();
+        });
+    }
+
+    public List<ContactData> getContactsInGroup(GroupData group) {
+        return sessionFactory.fromSession(session -> {
+            return convertContactList(session.get(GroupRecord.class, group.id()).contacts);
+        });
     }
 }
--- addressbook_web_tests/src/test/java/ru/apatch/addressbook/manager/hbm/ContactRecord.java
+++ addressbook_web_tests/src/test/java/ru/apatch/addressbook/manager/hbm/ContactRecord.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Date;
+
 
 @Entity
 @Table(name = "addressbook")
@@ -28,4 +30,49 @@ public class ContactRecord {
 
     @Column(name = "email")
     public String email;
+
+    public String middlename = "";
+    public String nickname = "";
+    public String company = "";
+    public String title = "";
+    public String work = "";
+    public String home = "";
+    public String fax = "";
+    public String email1 = "";
+    public String email2 = "";
+    public String inm = "";
+    public String inm2 = "";
+    public String inm3 = "";
+    public String homepage = "";
+    public int birday = 18;
+    public int bmonth = 12;
+    public int year = 1992;
+    public int aday = 1;
+    public int amonth = 3;
+    public int ayear = 12;
+    public String address2 = "";
+    public String phone2 = "";
+    public String notes = "";
+    public String photo = "";
+    public String x_vcard = "";
+    public String x_activesync = "";
+    public Date created = new Date();
+    public Date modified = new Date();
+    public String role = "";
+    public String addr_long = "";
+    public String addr_lat = "";
+    public String addr_status = "";
+
+    public ContactRecord() {
+    }
+
+    public ContactRecord(int id, String firstname, String lastname, String address, String mobile, String email) {
+
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+    }
 }
